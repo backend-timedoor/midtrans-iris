abf8de1468a1bb938fdacc04afc26b8079d9a09a
Adi Perdana <dewaadiperdana@gmail.com>
2022-03-31 10:15:30 +0800

    add new plain request dto

---
+++ src/Dto/PlainRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Timedoor\TmdMidtransIris\Dto;
+
+use JsonSerializable;
+
+class PlainRequest implements JsonSerializable
+{
+    /**
+     * The request body
+     *
+     * @var array
+     */
+    private $body;
+
+    public function jsonSerialize()
+    {
+        return $this->body; 
+    }
+
+    /**
+     * Get the request body
+     *
+     * @return  array
+     */ 
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set the request body
+     *
+     * @param  array  $body  The request body
+     *
+     * @return  self
+     */ 
+    public function setBody(array $body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+}
